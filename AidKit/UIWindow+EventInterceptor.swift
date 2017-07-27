e5671cc491a87575733a6ebdbc9f68780f9a4f3b
Maher Rezeq <maher.rezeq@gmail.com>
2017-07-27 14:50:48 -0400

    Add UIWindow+EventInterceptor, Swizzled "SendEvent" method on UIWindow object.

---
+++ AidKit/UIWindow+EventInterceptor.swift
@@ -0,0 +1,51 @@
+//
+//  UIWindow+EventInterceptor.swift
+//  AidKit
+//
+//  Created by Rezeq, Maher on 7/27/17.
+//  Copyright © 2017 NativeNCreative. All rights reserved.
+//
+
+import UIKit
+
+fileprivate var isSwizzled = false
+
+@available(iOS 8.0, *)
+extension UIWindow {
+
+    public func swizzler() {
+
+        guard !isSwizzled else {
+            return
+        }
+
+        let sendEvent = class_getInstanceMethod(object_getClass(self), #selector(UIApplication.sendEvent(_:)))
+        let swizzledSendEvent = class_getInstanceMethod(object_getClass(self), #selector(UIWindow.swizzledSendEvent(_:)))
+        method_exchangeImplementations(sendEvent, swizzledSendEvent)
+
+        isSwizzled = true
+    }
+
+    public func swizzledSendEvent(_ event: UIEvent) {
+        handle(event)
+        swizzledSendEvent(event)
+    }
+
+    private func handle(_ event: UIEvent) {
+
+        switch event.type {
+
+        case .touches: break
+
+        case .motion:  break
+
+        case .presses: break
+
+        case .remoteControl: break
+            
+        default: break
+        }
+    }
+}
+
+
